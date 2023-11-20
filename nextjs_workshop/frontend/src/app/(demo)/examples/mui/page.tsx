64f5f5ce712c0fb59b79d3b27044f4505b24078e
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-20 15:21:24 +0700

    1234

--- nextjs_workshop/frontend/src/app/(demo)/examples/mui/page.tsx
+++ nextjs_workshop/frontend/src/app/(demo)/examples/mui/page.tsx
@@ -3,5 +3,9 @@ import React from "react";
 type Props = {};
 
 export default function MuiPage({}: Props) {
-  return <div className="text-red-400">MuiPage</div>;
+  return (
+    <div className="text-red-400 bg-yellow-500 w-[100px] m-3 text-center rounded-md">
+      MuiPage
+    </div>
+  );
 }
