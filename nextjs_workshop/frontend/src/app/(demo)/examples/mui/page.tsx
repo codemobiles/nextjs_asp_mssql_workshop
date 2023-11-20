4f7871e438d0bdde4b473427e7dd4f2117067e84
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-20 15:44:29 +0700

    1234

--- nextjs_workshop/frontend/src/app/(demo)/examples/mui/page.tsx
+++ nextjs_workshop/frontend/src/app/(demo)/examples/mui/page.tsx
@@ -1,23 +1,29 @@
 import { Stack, Button } from "@mui/material";
+import { red } from "@mui/material/colors";
 import React from "react";
 
 type Props = {};
 
 export default function MuiPage({}: Props) {
+  //   BasicButtons({ lable: "1234" });
+
   return (
     <div>
-      <BasicButtons />
+      <BasicButtons lable="Hey" color="purple" />
     </div>
   );
 }
 
 type BasicButtonsProps = {
   lable: string;
+  color: string;
 };
-function BasicButtons(props: BasicButtonsProps) {
+function BasicButtons({ lable, color }: BasicButtonsProps) {
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="contained">{props.lable}</Button>
+      <Button variant="contained" style={{ backgroundColor: color }}>
+        {lable}
+      </Button>
     </Stack>
   );
 }
