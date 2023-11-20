443f8ea3011f0b99c8f6624f9e9e12f08f916247
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-20 15:28:21 +0700

    1234

--- nextjs_workshop/frontend/src/app/(demo)/examples/mui/page.tsx
+++ nextjs_workshop/frontend/src/app/(demo)/examples/mui/page.tsx
@@ -9,3 +9,19 @@ export default function MuiPage({}: Props) {
     </div>
   );
 }
+
+
+
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+export default function BasicButtons() {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
+}
