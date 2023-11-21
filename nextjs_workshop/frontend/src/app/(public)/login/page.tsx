c498d03d27ebcd8d0ee491fdcfcc2a286f51ae4c
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-21 10:36:10 +0700

    1234

--- nextjs_workshop/frontend/src/app/(public)/login/page.tsx
+++ nextjs_workshop/frontend/src/app/(public)/login/page.tsx
@@ -12,7 +12,13 @@ export default function LoginPage({}: Props) {
           alert("Hey");
         }}
       >
-        <input type="text" name="username" placeholder="Username" /> <br />
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={(e) => console.log(e.target.value)}
+        />{" "}
+        <br />
         <input type="text" name="password" placeholder="Password" /> <br />
         <button>Submit</button>
       </form>
