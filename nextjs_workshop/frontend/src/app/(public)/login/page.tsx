f2c65c12f5dfe96905ac5763145ab182026b55c4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-21 13:43:30 +0700

    1234

--- nextjs_workshop/frontend/src/app/(public)/login/page.tsx
+++ nextjs_workshop/frontend/src/app/(public)/login/page.tsx
@@ -1,10 +1,10 @@
-
 import { Button, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Metadata } from "next";
+import LoginForm from "@/app/_components/pages/login/LoginForm";
 
 export const metadata: Metadata = {
   title: "Login page",
@@ -25,66 +25,5 @@ const defaultValues: User = {
 };
 
 export default function LoginPage({}: Props) {
-  const formValidateSchema = Yup.object().shape({
-    username: Yup.string().required("Username is required").trim(),
-    password: Yup.string().required("Password is required").trim(),
-  });
-
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<User>({
-    defaultValues,
-    resolver: yupResolver(formValidateSchema),
-  });
-
-  return (
-    <div>
-      <Typography variant="h2" className="mb-5">
-        LoginPage
-      </Typography>
-      <form
-        onSubmit={handleSubmit((values) => {
-          alert(JSON.stringify(values));
-        })}
-      >
-        {/* Username */}
-        <Controller
-          name="username"
-          control={control}
-          render={({ field }) => (
-            <TextField
-              error={Boolean(errors.username?.message)}
-              helperText={errors.username?.message}
-              variant="outlined"
-              label="Username"
-              {...field}
-            />
-          )}
-        />
-        <br />
-        {/* Password */}
-        <Controller
-          name="password"
-          control={control}
-          render={({ field }) => (
-            <TextField
-              error={Boolean(errors.password?.message)}
-              helperText={errors.password?.message}
-              sx={{ mt: 3 }}
-              variant="outlined"
-              label="Password"
-              {...field}
-            />
-          )}
-        />
-        <br />
-
-        <Button type="submit" variant="contained">
-          Submit
-        </Button>
-      </form>
-    </div>
-  );
+  return <LoginForm />;
 }
---
+++ nextjs_workshop/frontend/src/app/_components/pages/login/LoginForm.tsx
@@ -0,0 +1,151 @@
+"use client";
+
+import {
+  Alert,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
+import * as Icons from "@mui/icons-material/";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
+
+interface User {
+  username: string;
+  password: string;
+}
+
+export default function LoginForm() {
+  const router = useRouter();
+  const initialValue: User = { username: "admin", password: "" };
+  const formValidateSchema = Yup.object().shape({
+    username: Yup.string().required("Username is required").trim(),
+    password: Yup.string().required("Password is required").trim(),
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>({
+    defaultValues: initialValue,
+    resolver: yupResolver(formValidateSchema),
+  });
+
+  const showForm = () => {
+    return (
+      <form onSubmit={handleSubmit(async (value: User) => {})}>
+        {/* Username */}
+        <Controller
+          name="username"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              error={(errors.username?.message ?? "") != ""}
+              helperText={errors.username?.message?.toString()}
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icons.Email />
+                  </InputAdornment>
+                ),
+              }}
+              label="Username"
+              autoComplete="email"
+              autoFocus
+            />
+          )}
+        />
+
+        {/* Password */}
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              type="password"
+              error={(errors.password?.message ?? "") != ""}
+              helperText={errors.password?.message?.toString()}
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icons.Password />
+                  </InputAdornment>
+                ),
+              }}
+              label="Password"
+              autoComplete="password"
+              autoFocus
+            />
+          )}
+        />
+
+        <Button
+          className="mt-8"
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+        >
+          Login
+        </Button>
+
+        <Button
+          className="mt-4"
+          onClick={() => { }}
+          type="button"
+          fullWidth
+          variant="outlined"
+        >
+          Register
+        </Button>
+      </form>
+    );
+  };
+
+  return (
+    <Box className="flex justify-center items-center">
+      <Card className="max-w-[345px] mt-[100px]">
+        <CardMedia
+          sx={{ height: 200 }}
+          image="/static/img/next_login.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          {/* <Typography gutterBottom variant="h5" component="h2">
+            Login ({reducer.count})
+          </Typography> */}
+          {showForm()}
+        </CardContent>
+      </Card>
+      <style jsx global>
+        {`
+          body {
+            min-height: 100vh;
+            position: relative;
+            margin: 0;
+            background-size: cover;
+            background-image: url("/static/img/bg4.jpg");
+            text-align: center;
+          }
+        `}
+      </style>
+    </Box>
+  );
+}
