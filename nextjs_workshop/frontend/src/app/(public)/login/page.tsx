f1019ad6ce6c305a8b8b77a0c01057ff64b266c4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-21 10:41:51 +0700

    1234

--- nextjs_workshop/frontend/src/app/(public)/login/page.tsx
+++ nextjs_workshop/frontend/src/app/(public)/login/page.tsx
@@ -3,7 +3,15 @@ import React from "react";
 
 type Props = {};
 
+type User = {
+  username: string;
+  password: string;
+  type?: string;
+};
+
 export default function LoginPage({}: Props) {
+  let user: User = { username: "", password: "" };
+
   return (
     <div>
       LoginPage
@@ -17,9 +25,11 @@ export default function LoginPage({}: Props) {
           name="username"
           placeholder="Username"
           onChange={(e) => console.log(e.target.value)}
-        />{" "}
+        />
         <br />
         <input type="text" name="password" placeholder="Password" /> <br />
+        <span>#Spy{JSON.stringify(user)}</span>
+        <br />
         <button>Submit</button>
       </form>
     </div>
