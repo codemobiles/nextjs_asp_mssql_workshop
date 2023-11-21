f2bf828ea565c25a08c0d0c1d2abb33f88d3d524
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-21 10:26:28 +0700

    1234

---
+++ nextjs_workshop/frontend/src/app/(protected)/layout.tsx
@@ -0,0 +1,73 @@
+"use client";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Box, CssBaseline, styled, useTheme } from "@mui/material";
+import React from "react";
+import Header from "../_components/layout/Header";
+import SideBar from "../_components/layout/Sidebar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <Header open={open} handleDrawerOpen={handleDrawerOpen} />
+          <SideBar open={open} handleDrawerClose={handleDrawerClose} />
+          <Main open={open}>
+            <DrawerHeader />
+            {children}
+          </Main>
+        </Box>
+      </body>
+    </html>
+  );
+}
