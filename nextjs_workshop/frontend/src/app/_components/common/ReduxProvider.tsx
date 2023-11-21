c7fc964f66601ebdded29964872b8d3c0ca3802f
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-21 15:46:44 +0700

    1234

--- nextjs_workshop/frontend/src/app/_components/common/ReduxProvider.tsx
+++ nextjs_workshop/frontend/src/app/_components/common/ReduxProvider.tsx
@@ -1,9 +1,12 @@
+"use client";
+import { store } from "@/store/store";
 import React from "react";
+import { Provider } from "react-redux";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function ReduxProvider({ children }: Props) {
-  return <div>{children}</div>;
+  return <Provider store={store}>{children}</Provider>;
 }
--- nextjs_workshop/frontend/src/store/slices/userSlice.ts
+++ nextjs_workshop/frontend/src/store/slices/userSlice.ts
@@ -2,8 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
   name: "user",
-  reducers: {},
+  reducers: {
+    add: (state) => {
+      state.count++;
+    },
+    remove: (state) => {
+      state.count--;
+    },
+  },
   initialState: { count: 0 },
 });
 
 export default userSlice.reducer;
+export const { add, remove } = userSlice.actions;
