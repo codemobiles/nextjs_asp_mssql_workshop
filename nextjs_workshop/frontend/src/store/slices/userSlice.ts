01c2ad415339b04a58f021763840f1f508c04b23
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-22 10:26:34 +0700

    1234

--- nextjs_workshop/frontend/src/store/slices/userSlice.ts
+++ nextjs_workshop/frontend/src/store/slices/userSlice.ts
@@ -1,14 +1,17 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const addAsync = createAsyncThunk("addAsync", async () => {
+export const addAsync = createAsyncThunk("user/addAsync", async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return 2;
 });
 
-const resetAsync = async (resetValue: number) => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  return resetValue;
-};
+export const resetAsync = createAsyncThunk(
+  "user/resetAsync",
+  async (resetValue: number) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return resetValue;
+  }
+);
 
 const userSlice = createSlice({
   name: "user",
@@ -23,6 +26,10 @@ const userSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(addAsync.fulfilled, (state, action) => {
       state.count = state.count + action.payload;
+    });
+
+    builder.addCase(resetAsync.fulfilled, (state, action) => {
+      state.count = action.payload;
     });
   },
   initialState: { count: 10 },
