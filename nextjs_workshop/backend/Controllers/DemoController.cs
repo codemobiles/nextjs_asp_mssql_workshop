cfed65b02c9d5df65d4dd605f74cf6e9dfdb96c4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-22 14:22:00 +0700

    1234

--- nextjs_workshop/backend/Controllers/DemoController.cs
+++ nextjs_workshop/backend/Controllers/DemoController.cs
@@ -1,1 +1,20 @@
-api-con
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        public DemoController()
+        {
+        }
+
+
+    }
+}
