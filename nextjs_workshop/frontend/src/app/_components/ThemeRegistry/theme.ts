ec94298a57d15cffe0576090861514bff6ecfba3
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-22 11:28:57 +0700

    1234

--- nextjs_workshop/frontend/src/app/_components/ThemeRegistry/theme.ts
+++ nextjs_workshop/frontend/src/app/_components/ThemeRegistry/theme.ts
@@ -2,6 +2,8 @@ import { Roboto } from "next/font/google";
 import { createTheme } from "@mui/material/styles";
 import { blue, orange, red } from "@mui/material/colors";
 
+// MUI Style
+// https://www.youtube.com/watch?v=dmjHm5Gx0Yg&list=PLjPfp4Ph3gBpbVPivsoIaOhfeNwmBskI8
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
@@ -9,44 +11,7 @@ const roboto = Roboto({
 });
 const drawerWidth = 240;
 const theme = createTheme({
-  components: {
-    MuiDrawer: {
-      styleOverrides: {
-        paper: {
-          backgroundImage: 'url("/static/img/background_menu.jpg")',
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "bottom",
-          width: drawerWidth,
-        },
-      },
-    },
-    MuiAlert: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          ...(ownerState.severity === "info" && {
-            backgroundColor: "#60a5fa",
-          }),
-        }),
-      },
-    },
-  },
-  typography: {
-    fontFamily: "Chakra Petch",
-    // fontFamily: "Roboto",
-    fontWeightLight: 100,
-    fontWeightRegular: 400,
-    fontWeightMedium: 500,
-    fontWeightBold: 600,
-  },
-  spacing: 4,
-  palette: {
-    mode: "light",
-    primary:
-      process.env.NEXT_PUBLIC_IS_PRODUCTION == "0" ? { main: blue[900] } : blue,
-    background: {
-      default: "#FFF",
-    },
-  },
+  spacing: 8,
 });
 
 export default theme;
