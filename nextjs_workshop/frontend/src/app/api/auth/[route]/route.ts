1812ea262b56487a3882267250ac52b5e6b246a3
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-22 13:22:54 +0700

    1234

--- nextjs_workshop/frontend/src/app/api/auth/[route]/route.ts
+++ nextjs_workshop/frontend/src/app/api/auth/[route]/route.ts
@@ -12,10 +12,23 @@ export async function POST(
   const route = context.params.route;
   const body = await request.json();
   if (route === "signin") {
-    return NextResponse.json({
-      result: "ok",
-      message: "login successfully",
-      body,
-    });
+    return signin(body);
+  }
+}
+
+async function signin(body: {
+  username: string;
+  password: string;
+}): Promise<any> {
+  try {
+    const { username, password } = body;
+
+    if (username == "admin" && password == "1234") {
+      return NextResponse.json({ result: "ok", token: "1234" });
+    } else {
+      return NextResponse.json({ result: "nok" });
+    }
+  } catch (error: any) {
+    return NextResponse.json({ result: "nok" });
   }
 }
