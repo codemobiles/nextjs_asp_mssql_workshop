81296b02e946af6d8027eed435b9e6154c10c814
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-23 11:11:21 +0700

    1234

---
+++ nextjs_workshop/backend/Installers/InstallerExtensions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace backend.Installers
+{
+    public static class InstallerExtensions
+    {
+
+    }
+}
--- nextjs_workshop/backend/Program.cs
+++ nextjs_workshop/backend/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Install
 
 
 builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionSQLServer")));
