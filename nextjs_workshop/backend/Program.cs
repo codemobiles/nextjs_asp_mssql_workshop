04ecf92a6e4ab0c8ca7539be2ee4084f3cc3a8f3
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-23 13:44:33 +0700

    1234

--- nextjs_workshop/backend/Program.cs
+++ nextjs_workshop/backend/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using backend.Database;
 using backend.Installers;
+using Autofac;
+using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +15,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.InstallServiceInAssembly(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
+
+// Option 1# to Add services (Repository)
+// builder.Services.AddTransient<IProductRepository, ProductRepository>();
+// builder.Services.AddTransient<IAuthRepository, AuthRepository>();
+
+// Option 2# to Auto Add Services
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly())
+    .Where(t => t.Name.EndsWith("Repository")) // Suffix Naming of AuthRepository, ProductRepository
+    .AsImplementedInterfaces());
+
 
 
 var app = builder.Build();
