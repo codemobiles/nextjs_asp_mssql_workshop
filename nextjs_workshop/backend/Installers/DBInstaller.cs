e92441764fa7d90c8ebe8e035ea8a3f8084e4d01
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-23 11:30:50 +0700

    1234

--- nextjs_workshop/backend/Installers/DBInstaller.cs
+++ nextjs_workshop/backend/Installers/DBInstaller.cs
@@ -9,6 +9,25 @@ namespace backend.Installers
     {
         public void InstallServices(IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<DatabaseContext>(options =>
+                        options.UseSqlServer(configuration.GetConnectionString("ConnectionSQLServer")));
+
+            // services.AddDbContext<DatabaseContext>(options =>
+            //     options.UseMySql(configuration.GetConnectionString("ConnectionMySQL")));
+
+            // services.AddDbContext<DatabaseContext>(options =>
+            //     options.UseSqlite(configuration.GetConnectionString("ConnectionSQLite")));
+
+            // services.AddDbContext<DatabaseContext>(options =>
+            //  options.UseOracle(configuration.GetConnectionString("ConnectionORACLE")));
+
+            // Create Database when Database not found (code first concept)
+            // var serviceProvider = services.BuildServiceProvider();
+            // DBinitialize.INIT(serviceProvider);
+
+            // <PackageReference Include="Pomelo.EntityFrameworkCore.MySql" Version="2.2.6" />
+            // <PackageReference Include="Microsoft.EntityFrameworkCore.Sqlite" Version="2.0.0" />
+            // <PackageReference Include="Oracle.EntityFrameworkCore" Version="2.19.50" />
 
         }
     }
