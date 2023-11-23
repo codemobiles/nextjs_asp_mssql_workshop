71fceafe8b6c5523ed68625431a8aec738da82de
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-23 15:28:19 +0700

    1234

--- nextjs_workshop/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ nextjs_workshop/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d8f94a8c67d56f8f0eda3a5836c0a72a0a08ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+750c246dd58c37602702bff238ca9ef9c9601a2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
