032d709b88282c63aeeba99b50e7e8c21d17b166
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-23 10:53:31 +0700

    1234

---
+++ nextjs_workshop/backend/Controllers/v1/ProductController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+        }
+
+
+    }
+}
--- nextjs_workshop/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ nextjs_workshop/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b21e4fa5779e032bbfd3b5e1ea482c485dabe90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
