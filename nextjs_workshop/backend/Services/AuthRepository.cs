4229ad96d5ca53c7bdc9b49f16a591604cb4cc64
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-23 13:37:31 +0700

    1234

--- nextjs_workshop/backend/Services/AuthRepository.cs
+++ nextjs_workshop/backend/Services/AuthRepository.cs
@@ -1,5 +1,7 @@
+using System.Security.Cryptography;
 using backend.Database;
 using backend.Models;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace backend.Services
 {
@@ -21,6 +23,23 @@ namespace backend.Services
         public void Register(User user)
         {
             throw new NotImplementedException();
+        }
+
+
+        private string CreatePasswordHash(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+            return $"{Convert.ToBase64String(salt)}.{hashed}";
         }
     }
 
