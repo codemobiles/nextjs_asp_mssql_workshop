e6bd3fa0ee2cfc774ed3fd3cbef0f2aeddee8e70
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-24 11:43:25 +0700

    1234

--- nextjs_workshop/frontend/src/store/slices/productSlice.ts
+++ nextjs_workshop/frontend/src/store/slices/productSlice.ts
@@ -1,4 +1,13 @@
+import { ProductData } from "@/models/product.model";
 import { createSlice } from "@reduxjs/toolkit";
+
+interface ProductState {
+  products: ProductData[];
+}
+
+const initialState: ProductState = {
+  products: [],
+};
 
 const productSlice = createSlice({
   name: "product",
