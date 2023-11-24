a956b5edce7470f7433e4357b870206f5f4c5cbd
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-24 14:15:11 +0700

    1234

--- nextjs_workshop/frontend/next.config.js
+++ nextjs_workshop/frontend/next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    // domains: ["codemobiles.com", "pospos.co", "localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "codemobiles.com",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        pathname: "**",
+      },
+    ],
+    minimumCacheTTL: 0,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
