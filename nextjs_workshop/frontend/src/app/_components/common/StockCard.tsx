018a7a0159b53bd23c4add2e46859ac31145274e
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-24 15:39:46 +0700

    1234

--- nextjs_workshop/frontend/src/app/_components/common/StockCard.tsx
+++ nextjs_workshop/frontend/src/app/_components/common/StockCard.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  icon: any;
+  title: string;
+  subtitle: string;
+  color: string;
+};
 
 export default function StockCard({}: Props) {
   return <div>StockCard</div>;
