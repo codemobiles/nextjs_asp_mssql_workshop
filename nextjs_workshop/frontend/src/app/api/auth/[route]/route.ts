9403dcfaee45cc6ab606cbc44a6a7cc86b573e9c
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-24 10:34:40 +0700

    1234

--- nextjs_workshop/frontend/src/app/api/auth/[route]/route.ts
+++ nextjs_workshop/frontend/src/app/api/auth/[route]/route.ts
@@ -1,3 +1,6 @@
+import { ACCESS_TOKEN_KEY } from "@/utils/constant";
+import fetchInterceptor from "@/utils/fetchInterceptor";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 // POST
@@ -20,15 +23,30 @@ async function signin(body: {
   username: string;
   password: string;
 }): Promise<any> {
-  try {
-    const { username, password } = body;
+  // try {
+  //   const { username, password } = body;
 
-    if (username == "admin" && password == "1234") {
-      return NextResponse.json({ result: "ok", token: "1234" });
-    } else {
-      return NextResponse.json({ result: "nok" });
-    }
-  } catch (error: any) {
-    return NextResponse.json({ result: "nok" });
-  }
+  //   if (username == "admin" && password == "1234") {
+  //     return NextResponse.json({ result: "ok", token: "1234" });
+  //   } else {
+  //     return NextResponse.json({ result: "nok" });
+  //   }
+  // } catch (error: any) {
+  //   return NextResponse.json({ result: "nok" });
+  // }
+
+
+   try {
+     const response = await fetchInterceptor.post(`/auth/login`, body);
+     const { token } = response;
+     cookies().set(ACCESS_TOKEN_KEY, token, {
+       secure: true,
+       sameSite: "strict",
+       path: "/",
+     });
+
+     return NextResponse.json(response);
+   } catch (error: any) {
+     return NextResponse.json({ result: "nok" });
+   }
 }
