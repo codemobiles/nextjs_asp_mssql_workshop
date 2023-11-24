26f18110a145734c1b5e2b72a084bfe671c601fc
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-24 11:18:49 +0700

    1234

--- nextjs_workshop/frontend/src/middleware.ts
+++ nextjs_workshop/frontend/src/middleware.ts
@@ -1,9 +1,29 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { ACCESS_TOKEN_KEY } from "./utils/constant";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/register", request.url));
+  const cookieStore = cookies();
+  const accessTokenKey = cookieStore.get(ACCESS_TOKEN_KEY);
+  const path = request.nextUrl.pathname;
+
+  if (accessTokenKey && accessTokenKey.value) {
+    if (path == "/login" || path == "/register" || path == "/") {
+      return NextResponse.redirect(new URL("/stock", request.url));
+    }
+
+    return NextResponse.next({
+      request,
+    });
+  } else if (path != "/login" && path != "/register") {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next({
+    request,
+  });
 }
 
 export const config = {
