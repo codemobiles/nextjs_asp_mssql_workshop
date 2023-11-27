6e555282dced6f4809911681b43f73a719be19ef
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-27 11:21:52 +0700

    1234

--- nextjs_workshop/frontend/src/app/(protected)/shop/page.tsx
+++ nextjs_workshop/frontend/src/app/(protected)/shop/page.tsx
@@ -1,10 +1,30 @@
+// import fetchInterceptor from "@/utils/fetchInterceptor";
+// import React from "react";
+
+// type Props = {};
+
+// export default async function ShopPage({}: Props) {
+//   const products = await fetchInterceptor.get("/product");
+
+//   return <div>ShopPage: {JSON.stringify(products)}</div>;
+// }
+
+"use client";
 import fetchInterceptor from "@/utils/fetchInterceptor";
-import React from "react";
+import React, { useEffect } from "react";
 
 type Props = {};
 
-export default async function ShopPage({}: Props) {
-  const products = await fetchInterceptor.get("/product");
+export default function ShopPage({}: Props) {
+  const [products, setProducts] = React.useState([]);
+  useEffect(() => {
+    load();
+  });
+
+  async function load() {
+    const result = await fetchInterceptor.get("/product");
+    setProducts(result);
+  }
 
   return <div>ShopPage: {JSON.stringify(products)}</div>;
 }
