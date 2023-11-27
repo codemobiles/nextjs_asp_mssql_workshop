252987b55fe147c99feca83eecc38680334b83fd
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-27 11:23:40 +0700

    1234

--- nextjs_workshop/frontend/src/app/(protected)/shop/page.tsx
+++ nextjs_workshop/frontend/src/app/(protected)/shop/page.tsx
@@ -1,3 +1,4 @@
+import { ProductData } from "@/models/product.model";
 import fetchInterceptor from "@/utils/fetchInterceptor";
 import { Box } from "@mui/material";
 import React from "react";
@@ -9,7 +10,7 @@ export default async function ShopPage({}: Props) {
 
   return (
     <Box className="grid gap-2 grid-cols-fluid w-full">
-      {products.map((p) => (
+      {products.map((p: ProductData) => (
         <ProductCard key={p.productId} product={p} />
       ))}
     </Box>
