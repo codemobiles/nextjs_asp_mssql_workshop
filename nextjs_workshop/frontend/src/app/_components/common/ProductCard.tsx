eea432f89cc02242f5067f73c57e0f6ae64b0200
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-27 11:36:38 +0700

    1234

--- nextjs_workshop/frontend/src/app/_components/common/ProductCard.tsx
+++ nextjs_workshop/frontend/src/app/_components/common/ProductCard.tsx
@@ -1,9 +1,54 @@
+"use client";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import { ProductData } from "@/models/product.model";
-import { Button } from "@mui/material";
-import React from "react";
+import { productImageURL } from "@/utils/commonUtil";
+import { NumericFormat } from "react-number-format";
+import { CardActionArea } from "@mui/material";
+import Image from "next/image";
 
-type Props = { product: ProductData };
-
+type Props = {
+  product: ProductData;
+};
 export default function ProductCard({ product }: Props) {
-  return <Button>{product.name}</Button>;
+  return (
+    <Card sx={{ maxWidth: 345 }} elevation={7}>
+      <CardActionArea onClick={() => alert(JSON.stringify(product))}>
+        <CardMedia sx={{ height: 240, textAlign: "center" }}>
+          <Image
+            src={productImageURL(product.image)}
+            width={200}
+            height={200}
+            style={{ objectFit: "contain" }}
+            alt=""
+          />
+        </CardMedia>
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h6"
+            component="div"
+            className="line-clamp-2"
+          >
+            {product.name}
+          </Typography>
+          <Typography variant="body1">
+            <NumericFormat
+              value={product.price}
+              displayType={"text"}
+              thousandSeparator={true}
+              decimalScale={2}
+              fixedDecimalScale={true}
+              suffix=" - "
+            />
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
