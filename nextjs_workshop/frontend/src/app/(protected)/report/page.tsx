3bd402cb797c6488ef9889f18ba4171afb2858d6
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-27 13:25:57 +0700

    1234

--- nextjs_workshop/frontend/src/app/(protected)/report/page.tsx
+++ nextjs_workshop/frontend/src/app/(protected)/report/page.tsx
@@ -4,17 +4,19 @@ import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend,
 } from "chart.js";
-import { Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend
@@ -28,33 +30,34 @@ export const options = {
     },
     title: {
       display: true,
-      text: "Chart.js Bar Chart",
+      text: "Chart.js Line Chart",
     },
   },
 };
 
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
 function getRandomNumber() {
   return Math.floor(Math.random() * 101);
 }
-
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
 export const data = {
   labels,
   datasets: [
     {
       label: "Dataset 1",
       data: labels.map(() => getRandomNumber()),
+      borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
     {
       label: "Dataset 2",
       data: labels.map(() => getRandomNumber()),
+      borderColor: "rgb(53, 162, 235)",
       backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
   ],
 };
 
 export default function ReportPage() {
-  return <Bar options={options} data={data} />;
+  return <Line options={options} data={data} />;
 }
