da3ed4740ad34e8ba6cfd370f605b53dc65ffaa3
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-29 16:14:21 +0700

    1234

--- nextjs_workshop/tmp/next-demo/src/store/slices/appSlice.ts
+++ nextjs_workshop/tmp/next-demo/src/store/slices/appSlice.ts
@@ -1,9 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type AppState = {
+  products: any[];
+};
+
+const initialState: AppState = {
+  products: [],
+};
+
+export const load = () => {};
+
 const appSlice = createSlice({
   name: "app",
   reducers: {},
-  initialState: {},
+  initialState,
 });
 
 export default appSlice.reducer;
